94e5ab50e80528d7a5e37c6053a934e472ee2cba
filipeabmoura <filipemouradev@gmail.com>
2024-03-12 16:38:32 -0300

    add controleDeAcesso - logica e sting.contains

---
+++ controleDeAcesso/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+string permission = "Admin|Manager";
+int level = 55;
+
+if (permission.Contains("Admin"))
+{
+    if (level > 55)
+        Console.WriteLine("Welcome, Super Admin user.");
+    else
+        Console.WriteLine("Welcome, Admin user.");
+}
+else if ((permission.Contains("Manager")) && (level == 20))
+{
+    Console.WriteLine("Contact an Admin for access.");
+}
+else
+{
+    Console.WriteLine("You do not have sufficient privileges.");
+}

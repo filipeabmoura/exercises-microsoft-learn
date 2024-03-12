40227f7dfcef3a08901acd372051d52e56ed8c5e
filipeabmoura <filipemouradev@gmail.com>
2024-03-12 16:22:16 -0300

    add caraOuCoroa - operador ternário

---
+++ caraOuCoroa/Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+
+Random coin = new Random();
+int flip = coin.Next(2);
+string result = flip == 0 ? "heads" : "tails";
+
+Console.WriteLine(result);
--- caraOuCoroa/obj/Debug/net8.0/caraOuCoroa.AssemblyInfo.cs
+++ caraOuCoroa/obj/Debug/net8.0/caraOuCoroa.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("caraOuCoroa")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82179b6d2fc4f5ec923e275ed0bb8907e848292e")]
-[assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
-[assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
+[assembly: System.Reflection.AssemblyProductAttribute("caraOuCoroa")]
+[assembly: System.Reflection.AssemblyTitleAttribute("caraOuCoroa")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.

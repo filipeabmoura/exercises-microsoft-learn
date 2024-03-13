a7efcb64fe556b27aebfd4d68d6d335257bcb4eb
filipeabmoura <filipemouradev@gmail.com>
2024-03-13 16:06:50 -0300

    add entradaDados - do-while and while

---
+++ entradaDados/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+int num = 0;
+string? entrada;
+
+do
+{
+    Console.WriteLine("Digite um número inteiro entre 5 e 10:");
+    entrada = Console.ReadLine();
+    num = int.Parse(entrada);
+} while (num < 5 || num > 10);
+
+Console.WriteLine(num);

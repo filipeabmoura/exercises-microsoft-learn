71a3f07715ce7701a1980b961aebfa39b87d0806
filipeabmoura <filipemouradev@gmail.com>
2024-03-13 14:05:14 -0300

    add FizzBuzz - for loop and logic

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+for (int i = 1; i <= 100; i++){
+    if (i % 3 == 0)
+    {
+        if (i % 5 == 0)
+        {
+            //divisivel por 3 e 5
+            Console.WriteLine($"{i} - FizzBuzz");
+        }
+        else
+        {  
+            //divisivel apenas por 3
+            Console.WriteLine($"{i} - Fizz");
+        }
+    } 
+    else if (i % 5 == 0)
+    {
+        //divisivel apenas por 5
+        Console.WriteLine($"{i} - Buzz");
+    }
+    else {
+        //não é divisível por 3 e nem por 5
+        Console.WriteLine(i);
+    }
+}
--- FizzBuzz/obj/Debug/net8.0/FizzBuzz.AssemblyInfo.cs
+++ FizzBuzz/obj/Debug/net8.0/FizzBuzz.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("FizzBuzz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+903bbccf1c9f2c163c6d29f89e5aa91523ed0057")]
-[assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
-[assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
+[assembly: System.Reflection.AssemblyProductAttribute("FizzBuzz")]
+[assembly: System.Reflection.AssemblyTitleAttribute("FizzBuzz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
